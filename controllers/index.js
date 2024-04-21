cc5f7403e654921ce90bc1754abd0a2c2652a668
anastassi1a <akor5665@gmail.com>
2024-04-21 19:34:49 +0500

    добавила проект второй недели

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const defaultRouteController = require("./default")
+const gameRouteController = require("./game")
+const mainRouteController = require("./main")
+const voteRouteController = require("./vote")
+module.exports = {
+    mainRouteController,
+    gameRouteController,
+    defaultRouteController,
+    voteRouteController
+}
